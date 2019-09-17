47440e3825cf5626edbc1f1d9a608ba572c34e29
WAU\aubrey.boorer <aubrey.boorer@webapplicationsuk.com>
2019-09-17 01:16:56 +0100

    Update + Delete + Docs
    - Added update function.
    - Added delete function.
    - Gave all methods old and new documentation (basic).

--- AzureFunctionsCosmosDB/Routes/ClientRoute.cs
+++ AzureFunctionsCosmosDB/Routes/ClientRoute.cs
@@ -1,6 +1,8 @@
 ﻿using AzureFunctionsCosmosDB.Objects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -8,12 +10,20 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AzureFunctionsCosmosDB.Routes
 {
     public static class ClientRoute
     {
+        /// <summary>
+        /// Get all of the clients.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="clients"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
         [FunctionName("Clients")]
         public static IActionResult Clients(
             [HttpTrigger(
@@ -32,6 +42,13 @@ namespace AzureFunctionsCosmosDB.Routes
             return new OkObjectResult(clients);
         }
 
+        /// <summary>
+        /// Create a new client.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="clients"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
         [FunctionName("ClientCreate")]
         public static async Task<IActionResult> ClientCreate(
             [HttpTrigger(
@@ -64,8 +81,103 @@ namespace AzureFunctionsCosmosDB.Routes
             }
         }
 
-        [FunctionName("ClientById")]
-        public static IActionResult ClientById(
+        /// <summary>
+        /// Update a clients information.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="clients"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        [FunctionName("ClientUpdate")]
+        public static async Task<IActionResult> ClientUpdate(
+            [HttpTrigger(
+                AuthorizationLevel.Function,
+                "put",
+                Route = "clients/{id}"
+            )] HttpRequest req,
+            [CosmosDB(
+                ConnectionStringSetting = "CosmosDB"
+            )] DocumentClient documentClient,
+            ILogger log,
+            string id
+        )
+        {
+            using (StreamReader reader = new StreamReader(req.Body))
+            {
+                string requestBody = await reader.ReadToEndAsync();
+
+                Client updated = JsonConvert.DeserializeObject<Client>(requestBody);
+                Uri collectionUri = UriFactory.CreateDocumentCollectionUri("sql1", "Clients");
+                Document document = documentClient
+                    .CreateDocumentQuery(collectionUri)
+                    .Where(t => t.Id == id)
+                    .AsEnumerable()
+                    .FirstOrDefault();
+
+                if (document == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                if (!string.IsNullOrEmpty(updated.Forename))
+                    document.SetPropertyValue("Forename", updated.Forename);
+                if (!string.IsNullOrEmpty(updated.Surname))
+                    document.SetPropertyValue("Surname", updated.Surname);
+
+                await documentClient.ReplaceDocumentAsync(document);
+
+                return new OkObjectResult((dynamic)document);
+            }
+        }
+
+        /// <summary>
+        /// Delete a single client.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="documentClient"></param>
+        /// <param name="log"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [FunctionName("ClientDelete")]
+        public static async Task<IActionResult> ClientDelete(
+            [HttpTrigger(
+                AuthorizationLevel.Function,
+                "delete",
+                Route = "clients/{id}"
+            )] HttpRequest req,
+            [CosmosDB(
+                ConnectionStringSetting = "CosmosDB"
+            )] DocumentClient documentClient,
+            ILogger log,
+            string id
+        )
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("sql1", "Clients");
+            Document document = documentClient
+                .CreateDocumentQuery(collectionUri)
+                .Where(t => t.Id == id)
+                .AsEnumerable()
+                .FirstOrDefault();
+
+            if (document == null)
+            {
+                return new NotFoundResult();
+            }
+
+            await documentClient.DeleteDocumentAsync(document.SelfLink, new RequestOptions { PartitionKey = new PartitionKey(id) });
+
+            return new OkResult();
+        }
+
+        /// <summary>
+        /// Get a single client by their Id.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="client"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        [FunctionName("ClientGet")]
+        public static IActionResult ClientGet(
             [HttpTrigger(
                 AuthorizationLevel.Function,
                 "get",
@@ -81,6 +193,11 @@ namespace AzureFunctionsCosmosDB.Routes
             ILogger log
         )
         {
+            if (client == null)
+            {
+                return new NotFoundResult();
+            }
+
             return new OkObjectResult(client);
         }
     }
