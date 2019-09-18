bffa8b16621dd5806813ef61c20e7e2ce64fde73
Aubrey Boorer <aubrey.boorer@webapplicationsuk.com>
2019-09-18 18:34:19 +0100

    reee

--- AzureFunctionsCosmosDB/Objects/Client.cs
+++ AzureFunctionsCosmosDB/Objects/Client.cs
@@ -6,6 +6,7 @@ namespace AzureFunctionsCosmosDB.Objects
 {
     public class Client
     {
+        public int SafeId { get; set; }
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Forename { get; set; }
         public string Surname { get; set; }
